6ad8e688335034f40be8938ca167e1221ffd75ea
Rahul G <rahul.gudise@gmail.com>
2022-09-17 07:19:46 -0400

    important hotfix

--- hann.cpp
+++ hann.cpp
@@ -16,6 +16,6 @@ std::vector<std::vector<float>> hann(int dayCount, std::vector<float> samples) {
         }
         split.push_back(curVec); 
     }
-    bool cum = new bool(true);
+    bool cum = new int[0];
     return split;
 }
