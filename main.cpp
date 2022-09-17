4aa311485e71e2d287f116b95c1caac8174b6bb3
rishabh sambare <rishabhsamb@gmail.com>
2022-09-17 01:34:02 -0400

    good luck

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "Audiofile.h"
 using std::cout;
 using std::endl;
 
 int main() {
+    const std::string inputFilePath = "./test-audio.wav";
     std::cout << "Hello world!" << std::endl;
+        
+    AudioFile<float> a;
+    bool loadedOK = a.load (inputFilePath);
+    
+    /** If you hit this assert then the file path above
+     probably doesn't refer to a valid audio file */
+    assert (loadedOK);
 }
