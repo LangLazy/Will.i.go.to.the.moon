1252d9fb9dfffa0ab60bc3b4b0f3da28d9c55557
Rahul G <rahul.gudise@gmail.com>
2022-09-17 12:29:58 -0400

    added ticker loading logic

--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
 #include "Audiofile/Audiofile.h"
 #include "hann.h"
-#include "fft_settings.h"
-#include "fft.h"
 #include <iostream>
 #include <string>
+#include <sstream> 
+
+std::vector<std::string> getAllTickers(std::string path);
+
+void printStocks(std::string startDate);
+struct {             // Structure declaration
+  float price;         // Member (int variable)
+  std::string ticker;   // Member (string variable)
+} choiceStruct;
+
 
 int main() {
     const int DAYS = 260;
     const std::string inputFilePath = "./test-audio.wav";
-    const char * error = NULL;
-    typedef float real_type;
-    typedef std::complex<float> complex_type;
-    typedef std::vector<real_type> RealArray1D;
-    typedef std::vector<complex_type>  ComplexArray1D;
-
     AudioFile<float> a;
     bool loadedOK = a.load (inputFilePath);
     std::vector<std::vector<float>> split = hann(DAYS, a.samples[0]);
-    std::vector<std::vector<std::complex<float>>> fourierSplit;
-    for (int i = 0; i < split.size(); ++i) {
-        std::vector<std::complex<float>> l;
-        std::vector<float> m = split[i];
-        bool b;
-        l.resize(split[i].size());
-        b = simple_fft::FFT<RealArray1D, ComplexArray1D>(&m, &l, m.size(), error);
-        fourierSplit.push_back(l);
-        // b = simple_fft::FFT<std::vector<real_type>, std::vector<complex_type>(split[i], fourierSplit[i], split[i].size(), error);
-    }
+    std::vector<std::string> tickers = getAllTickers("tickers.csv");
+    std::cout << tickers.size() << std::endl;
     return 0;
 }
+
+std::vector<std::string> getAllTickers(std::string path){
+    std::ifstream file(path);
+    std::string holder;
+    std::vector<std::string> ans;
+    while (std::getline(file, holder, ',')){
+        ans.push_back(holder);
+    }
+    return ans;
+}
+
+// std::vector<choiceStruct> getTwo(){
+
+// }
