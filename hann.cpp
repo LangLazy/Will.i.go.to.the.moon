bd48d450e00b6bfe493ad4c0f809f8613501e5cc
Rahul G <rahul.gudise@gmail.com>
2022-09-17 06:54:26 -0400

    fixed namespace issue

--- hann.cpp
+++ hann.cpp
@@ -1,20 +1,21 @@
 #include<vector>
 #include <cmath>
 
-vector<vector<float>> hann(int dayCount, vector<float> samples) {
-    vector<vector<float>> split;
+std::vector<std::vector<float>> hann(int dayCount, std::vector<float> samples) {
+    std::vector<std::vector<float>> split;
     long interval = floor(samples.size()/dayCount);
     for (int i = 0; i < dayCount; i++){
         long numRepeats = interval;
         if (i == dayCount -1){
             numRepeats = samples.size() - i*interval;
         }
-        vector<float> curVec;
+        std::vector<float> curVec;
         for (int x = 0; x < numRepeats; x++) {
             double multiplier = 0.5 * (1 - cos(2*M_PI*x/interval));
             curVec.push_back(multiplier * samples[i*interval + x]);
         }
         split.push_back(curVec); 
     }
+    bool cum = new bool(true);
     return split;
 }
