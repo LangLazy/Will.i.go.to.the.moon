0ae674a9c2098958dd4589f14dc6fe7909d8d686
Rahul G <rahul.gudise@gmail.com>
2022-09-17 05:49:52 -0400

    initial hanning window code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include "Audiofile/Audiofile.h"
+#include <cmath>
+#include <math.h>
+using namespace std;
 
 int main() {
-    const std::string inputFilePath = "./test-audio.wav";
+    const string inputFilePath = "./test-audio.wav";
     AudioFile<float> a;
     bool loadedOK = a.load (inputFilePath);
     for (int i = 0; i < a.getNumSamplesPerChannel(); i++){
-        std::cout << a.samples[0][i] << std::endl;
+        cout << a.samples[0][i] << endl;
+    }
+    long interval = floor(a.getNumSamplesPerChannel()/260);
+    vector<vector<float>> split;
+    for (int i = 0; i < 260; i++){
+        vector<float> curVec;
+        if (i != 259){
+            for (int x = 0; x < interval; x++) {
+                double multiplier = 0.5 * (1 - cos(2*M_PI*x/interval));
+                curVec.push_back(multiplier * a.samples[0][i*interval + x]);
+            }
+            split.push_back(curVec);
+        } else{
+            long newInterval = a.samples[0].size() - i*interval;
+            for (int x = 0; x < newInterval; x++) {
+                double multiplier = 0.5 * (1 - cos(2*M_PI*x/newInterval));
+                curVec.push_back(multiplier * a.samples[0][i*interval + newInterval]);
+            }
+            split.push_back(curVec);
+        }        
+    }
+    for (int i = 0; i < split.size(); ++i) {
+        cout << i << " " << split[i].size() << endl;
     }
     return 0;
 }
